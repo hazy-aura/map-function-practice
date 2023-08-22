3f75bdc2d4f9a1952980ee944738859126d68ca6
Ayush Rana <108958206+hazy-aura@users.noreply.github.com>
2023-08-23 01:51:36 +0530

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import Entry from "./Entry";
+import emojipedia from "../emojipedia.js";
+
+function CreateEntry(emojipedi) {
+  return (
+    <Entry
+      emoji={emojipedi.emoji}
+      name={emojipedi.name}
+      meaning={emojipedi.meaning}
+    />
+  );
+}
+function App() {
+  return (
+    <div>
+      <h1>
+        <span>emojipedia</span>
+      </h1>
+
+      <dl className="dictionary">
+        {emojipedia.map(CreateEntry)}
+        {/* <Entry
+          emoji={emojipedia[0].emoji}
+          name={emojipedia[0].name}
+          meaning={emojipedia[0].meaning}
+        />
+        <Entry
+          emoji={emojipedia[1].emoji}
+          name={emojipedia[1].name}
+          meaning={emojipedia[1].meaning}
+        />
+        <Entry
+          emoji={emojipedia[2].emoji}
+          name={emojipedia[2].name}
+          meaning={emojipedia[2].meaning}
+        /> */}
+      </dl>
+    </div>
+  );
+}
+
+export default App;
